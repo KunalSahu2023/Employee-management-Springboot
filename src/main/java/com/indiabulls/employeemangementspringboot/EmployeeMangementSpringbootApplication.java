1b3f339729325fdb916d541d1cbf44279bd81bd7
Kunal Sahu <kunalsahu313@gmail.com>
2026-02-13 23:02:00 +0530

    project setup on feb

---
+++ src/main/java/com/indiabulls/employeemangementspringboot/EmployeeMangementSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.indiabulls.employeemangementspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMangementSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeMangementSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/indiabulls/employeemangementspringboot/EmployeeMangementSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indiabulls.employeemangementspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMangementSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
